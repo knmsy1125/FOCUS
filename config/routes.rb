beb4bfc527ea977e8fbcf2c92cc9cb9bcc8155c2
knmsy1125 <knmsy1125@gmail.com>
2021-05-08 07:31:01 +0000

    routes追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,25 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
+  
   root 'user/homes#top'
+  
+  scope module: :user do
+    resource :users, only: [:show, :edit, :update]
+    get 'users/unsubscribe' => 'users#unsubscribe'
+    delete 'users/withdraw'  => 'users#withdraw'
+    resources :goals, only: [:new, :create, :index, :show]
+    resources :tasks, only: [:index]
+  end
+    
+  scope module: :frend do
+    resources :frends, only: [:index] do
+      resources :comments, only: [:new, :create]
+      resources :fans, only: [:create]
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
